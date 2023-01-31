b5e7e2ea41c2f0d110862bc24a045338647c1979
Asif Iqubal <asif15iqubal@gmail.com>
2023-01-31 11:54:17 +0530

    task completed

--- rest/views.py
+++ rest/views.py
@@ -10,12 +10,10 @@ import os
 
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
+
 CLIENT_SECRETS_FILE = "credentials.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection and REDIRECT URL.
+
 SCOPES = ['https://www.googleapis.com/auth/calendar',
           'https://www.googleapis.com/auth/userinfo.email',
           'https://www.googleapis.com/auth/userinfo.profile',
@@ -31,20 +29,16 @@ def GoogleCalendarInitView(request):
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE, scopes=SCOPES)
 
-    # The URI created here must exactly match one of the authorized redirect URIs
-    # for the OAuth 2.0 client, which you configured in the API Console. If this
-    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
-    # error.
+    
     flow.redirect_uri = REDIRECT_URL
 
     authorization_url, state = flow.authorization_url(
-        # Enable offline access so that you can refresh an access token without
-        # re-prompting the user for permission. Recommended for web server apps.
+        
         access_type='offline',
-        # Enable incremental authorization. Recommended as a best practice.
+        
         include_granted_scopes='true')
 
-    # Store the state so the callback can verify the auth server response.
+    
     request.session['state'] = state
 
     return Response({"authorization_url": authorization_url})
@@ -52,8 +46,7 @@ def GoogleCalendarInitView(request):
 
 @api_view(['GET'])
 def GoogleCalendarRedirectView(request):
-    # Specify the state when creating the flow in the callback so that it can
-    # verified in the authorization server response.
+    
     state = request.session['state']
 
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
@@ -65,9 +58,7 @@ def GoogleCalendarRedirectView(request):
     flow.fetch_token(authorization_response=authorization_response)
 
 
-    # Save credentials back to session in case access token was refreshed.
-    # ACTION ITEM: In a production app, you likely want to save these
-    # credentials in a persistent database instead.
+    
     credentials = flow.credentials
     request.session['credentials'] = credentials_to_dict(credentials)
 
@@ -75,13 +66,11 @@ def GoogleCalendarRedirectView(request):
     if 'credentials' not in request.session:
         return redirect('v1/calendar/init')
 
-    # Load credentials from the session.
+    
     credentials = google.oauth2.credentials.Credentials(
         **request.session['credentials'])
 
-    # Use the Google API Discovery Service to build client libraries, IDE plugins,
-    # and other tools that interact with Google APIs.
-    # The Discovery API provides a list of Google APIs and a machine-readable "Discovery Document" for each API
+    
     service = googleapiclient.discovery.build(
         API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -2,22 +2,15 @@
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%%mj%z2hkrf!1wy0(l2!73$h85rsux*))+a1&00$8@#hchbe18'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'rest.apps.RestConfig',
@@ -61,10 +54,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -92,9 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Kolkata'
@@ -103,13 +89,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
